5df067a03d914ae3ab762814e8a59c50e8c769e2
johney <fengshaoming@fengshaomingde-MacBook-Pro.local>
2020-07-02 01:30:39 +0800

    test connect

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import flask
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Hello Flask!</h1>"
 
-
-app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=80)
